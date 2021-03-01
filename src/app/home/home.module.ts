9a600fcbc5edad36e0e61c4eb8203e9eefafe358
johnj974 <jjdug@hotmail.com>
2021-03-01 20:40:57 +0000

    course component added to home+base style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MainHomeComponent } from './main-home/main-home.component';
+import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
-  declarations: [MainHomeComponent],
+  declarations: [MainHomeComponent, CoursesComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
