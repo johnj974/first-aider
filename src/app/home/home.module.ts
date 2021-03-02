bd9258fa7d1ad18445435dbc158accfcaf8eebea
johnj974 <jjdug@hotmail.com>
2021-03-02 15:56:39 +0000

    add about component to home module+ styling

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { MainHomeComponent } from './main-home/main-home.component';
 import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
-  declarations: [MainHomeComponent, CoursesComponent],
+  declarations: [MainHomeComponent, CoursesComponent, AboutComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
