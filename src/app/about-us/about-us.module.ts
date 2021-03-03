ba93985285a1b46580c173ff422cb4eb504d00b5
johnj974 <jjdug@hotmail.com>
2021-03-03 19:06:40 +0000

    component added to aboutModule+start  layout+style

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { MainAboutComponent } from './main-about/main-about.component';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
+import { AdviceComponent } from './advice/advice.component';
 
 
 @NgModule({
-  declarations: [MainAboutComponent],
+  declarations: [MainAboutComponent, WhoWeAreComponent, WhatWeDoComponent, AdviceComponent],
   imports: [
     CommonModule,
     AboutUsRoutingModule
