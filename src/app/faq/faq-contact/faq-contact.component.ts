a28787376b14778a4f23e3d6742811bde365891a
johnj974 <jjdug@hotmail.com>
2021-03-18 20:54:45 +0000

    components added to faq section/some style applied

---
+++ src/app/faq/faq-contact/faq-contact.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-contact',
+  templateUrl: './faq-contact.component.html',
+  styleUrls: ['./faq-contact.component.scss'],
+})
+export class FaqContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onToContact() {}
+}
--- src/app/faq/faq.module.ts
+++ src/app/faq/faq.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { FaqRoutingModule } from './faq-routing.module';
 import { MainFaqComponent } from './main-faq/main-faq.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { FaqContactComponent } from './faq-contact/faq-contact.component';
 
 
 @NgModule({
-  declarations: [MainFaqComponent],
+  declarations: [MainFaqComponent, QuestionsComponent, FaqContactComponent],
   imports: [
     CommonModule,
     FaqRoutingModule
