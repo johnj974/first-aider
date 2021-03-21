7974194ab576939c6d31decef2ca1cf4d484810e
johnj974 <jjdug@hotmail.com>
2021-03-21 19:41:49 +0000

    added prepopulation to course booking form

--- src/app/courses/course-book/course-book.component.ts
+++ src/app/courses/course-book/course-book.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Coursemodel } from 'src/app/shared/models/course.model';
 import { TestimonialModel } from 'src/app/shared/models/testimonial.model';
+import { CoursesService } from 'src/app/shared/services/courses.service';
 import { TestimonialService } from 'src/app/shared/services/testimonial.service';
 
 @Component({
@@ -12,8 +15,15 @@ export class CourseBookComponent implements OnInit {
   //
   testimonialArray: TestimonialModel[];
   bookingForm: FormGroup;
+  courseId: string;
+  courseBookingInfo: Coursemodel;
+  test: string = 'hell';
 
-  constructor(private testimonialService: TestimonialService) {}
+  constructor(
+    private testimonialService: TestimonialService,
+    private courseService: CoursesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.testimonialArray = this.testimonialService.testimonialServiceArray;
@@ -21,13 +31,21 @@ export class CourseBookComponent implements OnInit {
     this.bookingForm = new FormGroup({
       course: new FormControl(null),
       location: new FormControl(null),
-      people: new FormControl(null, Validators.required),
+      date: new FormControl(null, Validators.required),
       cost: new FormControl(null),
       userData: new FormGroup({
         name: new FormControl(null, Validators.required),
         email: new FormControl(null, [Validators.required, Validators.email]),
         phone: new FormControl(null, Validators.required),
       }),
+    });
+
+    this.route.params.subscribe((params: Params) => {
+      this.courseId = params.id;
+      this.courseBookingInfo = this.courseService.onCourseDisplay(
+        this.courseId
+      );
+      console.log(this.courseBookingInfo);
     });
   }
 
