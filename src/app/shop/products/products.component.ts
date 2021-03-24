75a0bca7436c886deb58ed9ea1cb6302c8ae80f4
johnj974 <jjdug@hotmail.com>
2021-03-24 13:45:14 +0000

    content to shop section/product added to shop page

---
+++ src/app/shop/products/products.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductModel } from 'src/app/shared/models/product.model';
+import { ShopService } from 'src/app/shared/services/shop.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  //.
+  productArray: ProductModel[] = [];
+
+  constructor(private shopService: ShopService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.productArray = this.shopService.shopServiceArray;
+    console.log(this.productArray);
+  }
+
+  onToShop() {
+    this.router.navigate(['/shop']);
+  }
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
 import { MainShopComponent } from './main-shop/main-shop.component';
-
+import { ProductsComponent } from './products/products.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [MainShopComponent],
-  imports: [
-    CommonModule,
-    ShopRoutingModule
-  ]
+  declarations: [MainShopComponent, ProductsComponent],
+  imports: [CommonModule, ShopRoutingModule, SharedModule],
 })
-export class ShopModule { }
+export class ShopModule {}
