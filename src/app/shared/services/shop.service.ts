70245e3a15e585b084bebaa9effc9f173271bad8
johnj974 <jjdug@hotmail.com>
2021-03-30 19:02:59 +0100

    changed carousel direction,more detail to products

--- src/app/shared/services/shop.service.ts
+++ src/app/shared/services/shop.service.ts
@@ -12,50 +12,50 @@ export class ShopService {
     new ProductModel(
       'C1 Defibrillator',
       'assets/images/defibrillator.jfif',
-      'this is a defib',
+      'eum voluptatum nam',
       700
     ),
     new ProductModel(
       'Pocket Mask',
       'assets/images/pocket-mask.jpg',
-      'this is a pocket mask',
+      'Lorem ipsum dolor',
       5
     ),
     new ProductModel(
       'First Aid Kits',
       'assets/images/med-kit.jpg',
-      'this is a face mask',
-      3
+      'numquam nobis nihil!',
+      20
     ),
     new ProductModel(
       'Face Mask',
       'assets/images/masks.png',
-      'this is a face mask',
-      3
+      'Lorem ipsum dolor',
+      5
     ),
     new ProductModel(
       'Surgical Gloves',
       'assets/images/gloves.jpg',
-      'this is a face mask',
+      'Lorem ipsum dolork',
       10
     ),
     new ProductModel(
       'First Aid Signs',
       'assets/images/sign.png',
-      'this is a face mask',
+      'numquam nobis nihil!',
       5
     ),
     new ProductModel(
       'EpiPen',
       'assets/images/epi-pen.jpg',
-      'this is a face mask',
+      'Lorem ipsum dolor',
       30
     ),
     new ProductModel(
-      'Bandages+Slings',
+      'Slings+Bandages',
       'assets/images/bandages.jpg',
-      'this is a face mask',
-      3
+      'eum voluptatum nam',
+      10
     ),
   ];
 
